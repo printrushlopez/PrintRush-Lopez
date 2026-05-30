61888bac997dbcbef152a1daca80e933bfc8155a
printrushlopez <printrushlopez@gmail.com>
2026-05-30 17:21:51 +0800

    Add saveConfig method to electronAPI

--- desktop-agent/ui/preload.js
+++ desktop-agent/ui/preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   getEnv: () => ipcRenderer.invoke('get-env'),
   onBluetoothFileReceived: (callback) => ipcRenderer.on('bluetooth-file-received', (_event, fileInfo) => callback(fileInfo)),
-  printFile: (urlOrPath) => ipcRenderer.invoke('print-file', urlOrPath)
+  printFile: (urlOrPath) => ipcRenderer.invoke('print-file', urlOrPath),
+  saveConfig: (config) => ipcRenderer.invoke('save-config', config)
 });
